a29d1586f14b627b8dd23df451fdc22795ab7913
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-04-27 01:47:58 -0300

    Primeiro commit

---
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/SistemadeAgendamentodeConsultasApplication.java
@@ -0,0 +1,13 @@
+package com.antonio.SistemadeAgendamentodeConsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadeAgendamentodeConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadeAgendamentodeConsultasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonio/SistemadeAgendamentodeConsultas/SistemadeAgendamentodeConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonio.SistemadeAgendamentodeConsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadeAgendamentodeConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
