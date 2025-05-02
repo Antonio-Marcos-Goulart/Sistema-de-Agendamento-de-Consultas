453e4aed82dc5288aa2aa5ba19f9958189c9f585
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-05-02 18:55:19 -0300

    Criação classe cliente e implementações no construtor

--- src/main/java/com/antonio/SistemadeAgendamentodeConsultas/Paciente.java
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/Paciente.java
@@ -1,4 +1,18 @@
 package com.antonio.SistemadeAgendamentodeConsultas;
 
-public class Paciente {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Paciente extends Pessoa{
+
+    private String convenio;
+    private Boolean usoMedicacaoContinua;
+    private String problemaDeSaude;
+
+    public Paciente(int id, String nome, String cpf, String telefone, LocalDate dataNascimento, String endereco, byte statusCadastro, DateTimeFormatter dataFormatada, String convenio, Boolean usoMedicacaoContinua, String problemaDeSaude) {
+        super(id, nome, cpf, telefone, dataNascimento, endereco, statusCadastro, dataFormatada);
+        this.convenio = convenio;
+        this.usoMedicacaoContinua = usoMedicacaoContinua;
+        this.problemaDeSaude = problemaDeSaude;
+    }
 }
