8819e338ed262df52fc064c84609b96e7d2ea5cc
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-05-02 18:59:02 -0300

    criação da classe Paciente e implementações no construtor da mesma // ajustes na classe Pessoa

--- src/main/java/com/antonio/SistemadeAgendamentodeConsultas/Pessoa.java
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/Pessoa.java
@@ -16,17 +16,21 @@ public abstract class Pessoa {
     protected String telefone;
     protected LocalDate dataNascimento;
     protected String endereco;
+    protected byte statusCadastro;
     public DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public Pessoa() {
     }
 
-    public Pessoa(String nome, String cpf, String telefone, LocalDate dataNascimento, String endereco) {
+    public Pessoa(int id, String nome, String cpf, String telefone, LocalDate dataNascimento, String endereco, byte statusCadastro, DateTimeFormatter dataFormatada) {
+        this.id = id;
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
         this.dataNascimento = dataNascimento;
         this.endereco = endereco;
+        this.statusCadastro = 1;
+        this.dataFormatada = dataFormatada;
     }
 
     @Override
@@ -35,6 +39,8 @@ public abstract class Pessoa {
                 "\nCPF: " + cpf +
                 "\nTelefone: " + telefone +
                 "\nData de nascimento: " + dataNascimento.format(dataFormatada) +
-                "\nEndereço: " + endereco;
+                "\nEndereço: " + endereco +
+                "\nStatus: " + (statusCadastro == 1 ? "Ativo" : "Inativo"); // Usa o operador ternário para exibir "Ativo" se o status for 1, ou "Inativo" se for diferente
+
     }
 }
