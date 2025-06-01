6c1226dff818ab234d2cbd579f2818d7fe873acf
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-06-01 10:01:17 -0300

    ajustes

--- src/main/java/com/antonio/SistemadeAgendamentodeConsultas/model/Funcionario.java
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/model/Funcionario.java
@@ -15,20 +15,33 @@ public class Funcionario extends Pessoa{
     @Column(name = "funcao", nullable = false, unique = false)
     private String funcao;
 
+    @NotBlank(message = "Salário é obrigatória")
+    @Column(name = "salario", nullable = false, unique = false)
+    private double salario;
+
     public Funcionario() {
         super();
     }
 
-    public Funcionario(Long id, String nome, String cpf, String telefone, String email, LocalDate dataNascimento, LocalDate dataDeCadastro, String endereco, byte statusCadastro, String funcao) {
+    public Funcionario(Long id, String nome, String cpf, String telefone, String email, LocalDate dataNascimento, LocalDate dataDeCadastro, String endereco, byte statusCadastro, String funcao, double salario) {
         super(id, nome, cpf, telefone, email, dataNascimento, dataDeCadastro, endereco, statusCadastro);
         this.funcao = funcao;
+        this.salario = salario;
     }
 
     public String getFuncao() {
         return funcao;
     }
 
-    public void setFuncao (String funcao) {
+    public void setFuncao(String funcao) {
         this.funcao = funcao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
     }
 }
