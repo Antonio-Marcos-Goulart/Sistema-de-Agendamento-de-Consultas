16bde6966ffb2b57eaa1d63da0909bc7a9a11c6e
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-06-07 23:00:36 -0300

    Classe médico

--- src/main/java/com/antonio/SistemadeAgendamentodeConsultas/model/Medico.java
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/model/Medico.java
@@ -5,12 +5,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "medico", schema = "agendamento")
 public class Medico extends Funcionario{
 
     @NotBlank(message = "CRM é obrigatório")
+    @Pattern(
+            regexp = "^\\d{6}-[A-Z]{2}$",
+            message =  "CRM deve estar no formato 123456-SP (6 números, hífen e UF em letras maiúsculas)"
+    )
     @Column(name = "crm", nullable = false, unique = true)
     private String crm;
 
