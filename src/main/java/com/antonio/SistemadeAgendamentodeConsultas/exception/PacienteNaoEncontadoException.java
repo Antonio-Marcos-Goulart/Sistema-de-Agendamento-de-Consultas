e0dcff411af462bda74c1f1f7b98daa50a7d256c
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-07-13 22:39:50 -0300

    Adiciona exceção personalizada para paciente não encontrado

---
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/exception/PacienteNaoEncontadoException.java
@@ -0,0 +1,7 @@
+package com.antonio.SistemadeAgendamentodeConsultas.exception;
+
+public class PacienteNaoEncontadoException extends RuntimeException {
+    public PacienteNaoEncontadoException(String message) {
+        super(message);
+    }
+}
