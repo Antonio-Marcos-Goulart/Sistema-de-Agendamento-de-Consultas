d4c418be7f54bedbd7cf28d88a5ffbc52b0c0483
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-07-15 21:17:42 -0300

    Implementação da classe PacienteRepository.java

--- src/main/java/com/antonio/SistemadeAgendamentodeConsultas/repository/PacienteRepository.java
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/repository/PacienteRepository.java
@@ -1,7 +1,15 @@
 package com.antonio.SistemadeAgendamentodeConsultas.repository;
 
+import com.antonio.SistemadeAgendamentodeConsultas.model.Funcionario;
 import com.antonio.SistemadeAgendamentodeConsultas.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    List<Paciente> findByCpfContainingIgnoreCase(String cpf);
+    List<Paciente> findByNomeContainingIgnoreCase(String nome);
+    Optional<Paciente> findById(Long id);
 }
