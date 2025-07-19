2df84c3b98f6fa117d7ddda299767864505f7c2d
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-07-19 17:01:59 -0300

    Adiciona exceção personalizada MedicoNaoEncontradoExeption

---
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/exception/MedicoNaoEncontradoExeption.java
@@ -0,0 +1,7 @@
+package com.antonio.SistemadeAgendamentodeConsultas.exception;
+
+public class MedicoNaoEncontradoExeption extends RuntimeException {
+    public MedicoNaoEncontradoExeption(String message) {
+        super(message);
+    }
+}
