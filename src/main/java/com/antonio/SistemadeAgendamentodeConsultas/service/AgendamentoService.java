993c450db9782ce8cb8f714615e40e33db18db4e
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-08-10 20:28:00 -0300

    Salva o agendamento no banco via "Agendamento agendamentoSalvo = agendamentoRepository.save(agendamento);"
    Pega as informações do paciente que serão usadas para o envio do e-mail informando que a consulta foi agendada -- Usando o EmailService onde as informações serão colocadas em cada campo, montando o e-mail e após isso, chama o método "emailService.enviarEmail(para, assunto, conteudo);"

---
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/service/AgendamentoService.java
@@ -0,0 +1,33 @@
+package com.antonio.SistemadeAgendamentodeConsultas.service;
+
+import com.antonio.SistemadeAgendamentodeConsultas.model.Agendamento;
+import com.antonio.SistemadeAgendamentodeConsultas.repository.AgendamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendamentoService {
+
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
+
+    @Autowired
+    private EmailService emailService;
+    public Agendamento agendar(Agendamento agendamento){ // Recebe os dados do meu agendamento
+        Agendamento agendamentoSalvo = agendamentoRepository.save(agendamento); // Salva o agendamento no banco
+
+        // Envia o email para confirmar o agendamento com o paciente
+        String email = agendamentoSalvo.getPaciente().getEmail();
+        String assunto = "Confirmação de Agendamento";
+        String conteudo = "Olá " + agendamentoSalvo.getPaciente().getNome() + "\n" +
+                "Seu agendamento foi confirmado para o dia " + agendamento.getDataAgendamento() +
+                "Detalhes do agendamento:\n" +
+                "Médico: " + agendamentoSalvo.getMedico().getNome() + "\n" +
+                "Especialidade: " + agendamentoSalvo.getMedico().getFuncao() + "\n" +
+                "Local: " + agendamentoSalvo.getLocalConsulta();
+        emailService.enviarEmail(email, assunto, conteudo);
+
+        return agendamentoSalvo;
+    }
+
+}
