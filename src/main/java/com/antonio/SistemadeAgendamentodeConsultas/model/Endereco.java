d14a296f51f9ba00847165d7f3215d0428f2619e
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-08-14 12:52:30 -0300

    Adição do endereço completo no banco de dados, com rua, cidade... ficam salvos individualmente no banco de dados --  metodo anterior deu problemas na parte do endereço.
    Melhor manutenção com ela sendo uma classe independente

--- src/main/java/com/antonio/SistemadeAgendamentodeConsultas/model/Endereco.java
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/model/Endereco.java
@@ -1,5 +1,6 @@
 package com.antonio.SistemadeAgendamentodeConsultas.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -12,20 +13,26 @@ import lombok.Setter;
 public class Endereco {
 
     @NotBlank(message = "Rua não pode ser vazia")
+    @Column(name = "rua", nullable = false)
     private String rua;
 
     @NotBlank(message = "Número não pode ser vazio")
+    @Column(name = "numero", nullable = false)
     private String numero;
 
     @NotBlank(message = "Bairro não pode ser vazio")
+    @Column(name = "bairro", nullable = false)
     private String bairro;
 
     @NotBlank(message = "Cidade não pode ser vazia")
+    @Column(name = "cidade", nullable = false)
     private String cidade;
 
     @NotBlank(message = "Estado não pode ser vazio")
+    @Column(name = "estado", nullable = false)
     private String estado;
 
     @Pattern(regexp = "\\d{5}-\\d{3}", message = "CEP deve estar no formato 00000-000")
+    @Column(name = "cep", nullable = false)
     private String cep;
 }
