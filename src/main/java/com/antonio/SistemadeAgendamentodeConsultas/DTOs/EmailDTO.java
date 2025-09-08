c7979648eb3c6522104ed34f1831224a4243edb9
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-09-07 22:55:13 -0300

    EMAIL-DTO
    VERIFICAR IMPORTÂNCIA - ELE NÃO ACESSA NADA EM BANCO

---
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/DTOs/EmailDTO.java
@@ -0,0 +1,27 @@
+package com.antonio.SistemadeAgendamentodeConsultas.DTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDTO {
+
+    @NotNull
+    @NotBlank
+    private String destinatario;
+
+    @NotNull
+    @NotBlank
+    private String assunto;
+
+    @NotNull
+    @NotBlank
+    private String conteudo;
+
+    private String anexo;
+}
