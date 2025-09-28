0ae8828e9801fdcfe13b72dad2d26a45a7535ab0
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-09-28 19:32:09 -0300

    Representação dos dados sem expor a entidade JPA

---
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/DTOs/AgendamentoDTO.java
@@ -0,0 +1,31 @@
+package com.antonio.SistemadeAgendamentodeConsultas.DTOs;
+
+import com.antonio.SistemadeAgendamentodeConsultas.model.entidades.Agendamento;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AgendamentoDTO {
+    private Long id;
+    private LocalDateTime dtAgendamento;
+    private String localConsulta;
+    private String statusAgendamento;
+    private String tipoAgendamento;
+    private Long medicoId; // Apenas o ID do médico, evita serializar tudo
+
+    public AgendamentoDTO(Agendamento agendamento) {
+        this.id = agendamento.getId();
+        this.dtAgendamento = agendamento.getDataAgendamento();
+        this.localConsulta = agendamento.getLocalConsulta();
+        this.statusAgendamento = String.valueOf(agendamento.getStatusAgendamento());
+        this.tipoAgendamento = agendamento.getTipoAgendamento();
+        this.medicoId = agendamento.getMedico() != null ? agendamento.getMedico().getId() : null;
+    }
+}
