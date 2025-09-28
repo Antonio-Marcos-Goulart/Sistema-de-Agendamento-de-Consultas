76f7bb77a4c105c572809265ff5d5e8e399adb73
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-09-28 01:38:00 -0300

    deletado - simplificação do projeto

--- src/main/java/com/antonio/SistemadeAgendamentodeConsultas/exception/FuncionarioNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.antonio.SistemadeAgendamentodeConsultas.exception;
-
-public class FuncionarioNaoEncontradoException extends RuntimeException {
-    public FuncionarioNaoEncontradoException(String message) {
-        super(message);
-    }
-}
