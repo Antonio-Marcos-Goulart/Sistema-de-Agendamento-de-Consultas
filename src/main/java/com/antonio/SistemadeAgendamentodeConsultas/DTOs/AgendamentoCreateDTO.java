c528a0896ee237738d53f37fd3fa4e67d85f6894
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-11-09 10:45:44 -0300

    Criação do DTO para criação do agendamento com validação para os campos e formatação para a data de agendamento no padrão dd/MM/yyyy HH:mm:ss

---
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/DTOs/AgendamentoCreateDTO.java
@@ -0,0 +1,40 @@
+package com.antonio.SistemadeAgendamentodeConsultas.DTOs;
+
+import com.antonio.SistemadeAgendamentodeConsultas.enums.SituacaoAgendamento;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgendamentoCreateDTO {
+
+    private String observacoes;
+
+    @NotNull
+    @FutureOrPresent
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime dataAgendamento;
+
+    @NotBlank
+    private String localConsulta;
+
+    @NotBlank
+    private String tipoAgendamento;
+
+    @NotNull
+    private SituacaoAgendamento statusAgendamento;
+
+    @NotNull
+    private Long pacienteId;
+
+    @NotNull
+    private Long medicoId;
+}
