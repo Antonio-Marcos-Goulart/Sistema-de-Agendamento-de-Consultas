be3dd507ca0093c1ca623aa76e3fd6b9218bdaaf
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-11-09 10:36:18 -0300

    Criação da classe PacienteCreateDTO com validações para campos obrigatórios e formatação de data no padrão dd/MM/yyyy HH:mm:ss

---
+++ src/main/java/com/antonio/SistemadeAgendamentodeConsultas/DTOs/PacienteCreateDTO.java
@@ -0,0 +1,36 @@
+package com.antonio.SistemadeAgendamentodeConsultas.DTOs;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PacienteCreateDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{11}", message = "CPF deve ter 11 dígitos")
+    private String cpf;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{10,11}", message = "Telefone inválido")
+    private String telefone;
+
+    @Email(message = "E-mail inválido")
+    private String email;
+
+    @NotNull
+    @PastOrPresent
+    @JsonFormat(pattern = "dd/MM/yyyy", shape = JsonFormat.Shape.STRING)
+    private LocalDate dataNascimento;
+
+    @Valid
+    private EnderecoCreateDTO endereco;
+}
